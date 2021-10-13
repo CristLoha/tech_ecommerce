cb53262a81e38534eaf98e862f3269c3e205f5be
Christ Henry Loha <cristloha2205@gmail.com>
2021-10-13 15:05:11 +0800

    update theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color kPrimaryColor = Color(0xff5956E9);
+Color kSecondaryColor = Color(0xff58C0EA);
+Color kBackroundColor1 = Color(0xffFFFFFF);
+Color kBackgroundColor2 = Color(0xffE5E5E5);
+Color kBlackColor = Color(0xff000000);
+Color kGreyColor = Color(0xff868686);
