7f6190b2c34655c728ab1726c668800fbdb5e771
Christ Henry Loha <cristloha2205@gmail.com>
2021-10-13 15:13:27 +0800

    update fonts & colors

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color kPrimaryColor = Color(0xff5956E9);
 Color kSecondaryColor = Color(0xff58C0EA);
@@ -6,3 +7,27 @@ Color kBackroundColor1 = Color(0xffFFFFFF);
 Color kBackgroundColor2 = Color(0xffE5E5E5);
 Color kBlackColor = Color(0xff000000);
 Color kGreyColor = Color(0xff868686);
+
+TextStyle primaryTextStyle = GoogleFonts.raleway(
+  color: kBlackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.raleway(
+  color: kBackgroundColor2,
+);
+
+TextStyle greyTextStyle = GoogleFonts.raleway(
+  color: kGreyColor,
+);
+
+TextStyle blueTextStyle = GoogleFonts.raleway(
+  color: kPrimaryColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
