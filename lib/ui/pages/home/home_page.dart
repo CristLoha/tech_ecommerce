1f48fdbd9c84c86c102fdcdb5c8eb31c0f1fe685
Christ Henry Loha <cristloha2205@gmail.com>
2021-10-14 16:27:35 +0800

    update

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,36 +1,19 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:tech_ecommerce/shared/theme.dart';
-import 'package:tech_ecommerce/ui/widgets/navigation_drawer.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class HomePage extends StatefulWidget {
+import 'drawer_page/drawer_screen.dart';
+import 'drawer_page/home_screen.dart';
+
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kBackgroundColor2,
-      drawer: NavigationDrawer(),
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: kBackgroundColor2,
-        leading: Builder(
-          builder: (context) {
-            return IconButton(
-              icon: Image.asset(
-                'assets/icon_drawer.png',
-                width: 22,
-              ),
-              onPressed: () => Scaffold.of(context).openDrawer(),
-            );
-          },
-        ),
+      body: Stack(
+        children: [
+          DrawerScreen(),
+          HomeScreen(),
+        ],
       ),
     );
   }
