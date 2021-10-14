2adfb591a427003501371933b41dc8da9116e01d
Christ Henry Loha <cristloha2205@gmail.com>
2021-10-14 19:58:34 +0800

    update

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -9,6 +9,7 @@ Color kBackgroundColor1 = Color(0xffFFFFFF);
 Color kBackgroundColor2 = Color(0xffE5E5E5);
 Color kBlackColor = Color(0xff000000);
 Color kGreyColor = Color(0xff868686);
+Color kGreyColor2 = Color(0xffC9C9C9);
 
 TextStyle primaryTextStyle = GoogleFonts.raleway(
   color: kBlackColor,
